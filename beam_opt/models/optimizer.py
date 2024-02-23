units: corrected penalty/consumption unit mismatch

beam_opt/models/optimizer.py
@@ -98,9 +98,8 @@ class Optimizer:
         # Parameters set by set_parameters func
         self.delta = None
         self.budget = None
-        self.penalty_emission = 0
-        self.penalty_consumption = 0
-        self.penalty_flat = 0
+        self.penalty_usage = 0.0
+        self.penalty_flat = 0.0
         self.selected_df = None
 
         # Parameters set in optimize func
@@ -116,20 +115,16 @@ class Optimizer:
             self,
             budget,
             target,
-            penalty_emission,
-            penalty_consumption,
+            penalty_usage,
             penalty_flat,
             delta):
         self.delta = delta
         self.budget = np.array(budget)
-        self.penalty_emission = penalty_emission
-        self.penalty_consumption = penalty_consumption
+        self.penalty_usage = penalty_usage
         self.penalty_flat = penalty_flat
-
         target_df = pd.DataFrame({'Target': target, 'Year': self.timeline})
         target_df = target_df.merge(self.timeline_df, on='Year', how='right').fillna(method='ffill').set_index('Year')
-        setattr(self, LOOKUP[self.scenario]['target'], target_df.Target * 1000)  # target_df.Target is in mtCO2e, convert to kg
-
+        setattr(self, LOOKUP[self.scenario]['target'], target_df.Target)
         return {'status': 'success', 'message': ''}
 
     def _preselect(self, target_num=15, discard_thres=1e-3):
@@ -427,13 +422,7 @@ class Optimizer:
         :param excess: a numpy array or scalar to which penalties are applied
         :return: a numpy array or scalar of same shape as excess with applied penalties
         """
-        penalty = np.zeros_like(excess)
-        if self.scenario == 'Emission' and self.penalty_emission > 0:
-            penalty += np.where(excess > 0, excess, 0) * self.penalty_emission
-            penalty = penalty / 1000  # assume excess in kgCO2, penalty in mtCO2
-        elif self.scenario == 'Consumption' and self.penalty_consumption > 0:
-            penalty += np.where(excess > 0, excess, 0) * self.penalty_consumption
-
+        penalty = np.where(excess > 0, excess, 0) * self.penalty_usage
         flat_penalty = np.where(excess > 0, self.penalty_flat, 0)
         penalty += flat_penalty
         return penalty

beam_opt/utils/parse.py
@@ -397,7 +397,7 @@ def parse_beam_measures(property_view_id: int, emission_rates: dict, timeline: l
 
     baseline_elec = state.extra_data.get('Electricity Use - Grid Purchase (kBtu)', 0)
     baseline_gas = state.extra_data.get('Natural Gas Use (kBtu)', 0)
-    elec_mmbtu, gas_mmbtu = _baseline_fuel_mmbtu(timeline, baseline_elec, baseline_gas)
+    elec_kbtu, gas_kbtu = _baseline_fuel_kbtu(timeline, baseline_elec, baseline_gas)
     elec_co2, gas_co2 = _baseline_fuel_co2(
         emission_rates, timeline, baseline_elec, baseline_gas)
 
@@ -407,8 +407,8 @@ def parse_beam_measures(property_view_id: int, emission_rates: dict, timeline: l
         elec_co2,                                                           # Elec kgCO2
         gas_co2,                                                            # Gas kgCO2
         elec_co2 + gas_co2,                                                 # Total kgCO2
-        elec_mmbtu,                                                         # Elec (mmbtu)
-        gas_mmbtu,                                                          # Gas (mmbtu)
+        elec_kbtu,                                                          # Elec (kbtu)
+        gas_kbtu,                                                           # Gas (kbtu)
         state.extra_data.get('Electricity Bill Savings', 0),                # Electricity Bill Savings
         state.extra_data.get('Natural Gas Bill Savings', 0),                # Gas Bill Savings
     ]]
@@ -619,8 +619,8 @@ def _baseline_fuel_co2(emission_rates, timeline, baseline_elec, baseline_gas):
     return elec_use, gas_use
 
 
-def _baseline_fuel_mmbtu(timeline, baseline_elec, baseline_gas):
+def _baseline_fuel_kbtu(timeline, baseline_elec, baseline_gas):
     total_years = timeline[-1] - timeline[0] + 1
-    elec_mmbtu = np.full(total_years, float(baseline_elec) / 1000)
-    gas_mmbtu = np.full(total_years, float(baseline_gas) / 1000)
-    return elec_mmbtu, gas_mmbtu
+    elec_kbtu = np.full(total_years, float(baseline_elec))
+    gas_kbtu = np.full(total_years, float(baseline_gas))
+    return elec_kbtu, gas_kbtu

beam_opt/utils/validate.py
@@ -91,8 +91,7 @@ def validate_complete_data(complete_data: CompleteData, ids):
 def pre_validate_parameters(
         optimizer: Optimizer,
         budget, target,
-        penalty_emission,
-        penalty_consumption,
+        penalty_usage,
         penalty_flat,
         delta):
     """
@@ -110,9 +109,7 @@ def pre_validate_parameters(
         errors.append('Invalid input: must choose target > 0')
     if delta < 0 or delta > 1:  # discount factor
         errors.append('Invalid input: must choose delta in [0,1]')
-    if penalty_emission < 0 \
-            or penalty_consumption < 0 \
-            or penalty_flat < 0:
+    if penalty_usage < 0 or penalty_flat < 0:
         errors.append('Invalid input: must choose nonnegative penalty')
 
     # Check that Target is Achievable
@@ -122,9 +119,7 @@ def pre_validate_parameters(
 
     if optimizer.scenario in LOOKUP:
         lookup = LOOKUP[optimizer.scenario]
-        if np.isinf(penalty_emission) \
-                or np.isinf(penalty_consumption) \
-                or np.isinf(penalty_flat):
+        if np.isinf(penalty_usage) or np.isinf(penalty_flat):
             # Check whether target is achievable
             target = target_df.Target * optimizer.baseline[lookup['optimize']].iloc[0]
             max_reduction = optimizer.df.groupby('Group')[lookup['data']].max().sum()