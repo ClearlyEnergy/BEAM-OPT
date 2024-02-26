5895c2542735f26fbf4951f778f1ca059da8702b
Samuel Harris <sw.gleaves@gmail.com>
2024-02-26 12:44:52 -0500

    fix: use self-implemented explode in Optimizer constructor

--- beam_opt/models/optimizer.py
+++ beam_opt/models/optimizer.py
@@ -13,6 +13,7 @@ import pandas as pd
 import itertools
 
 from beam_opt.models.data_container import CompleteData
+from beam_opt.utils.utils import multi_col_explode
 
 LOGGER = logging.getLogger(__name__)
 ACCEPT_FIRST_SOLUTION = True
@@ -91,7 +92,7 @@ class Optimizer:
             LOOKUP[self.scenario]['optimize'],
         ]
 
-        self.baseline = self.baseline.explode(FUEL_COLS).reset_index(drop=True)
+        self.baseline = multi_col_explode(self.baseline, FUEL_COLS).reset_index(drop=True)
         self.baseline['Year'] = self.timeline_df
         self.baseline = self.baseline.set_index('Year')
 
--- beam_opt/utils/utils.py
+++ beam_opt/utils/utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def dump_optimizer_input(parsed_input, path=None):
     """
     Write or return a JSON string of CompleteData input
@@ -24,3 +27,24 @@ def dump_optimizer_result(scenarios, levels, path=None):
 
     with open(path, 'w') as f:
         f.write(result)
+
+
+def multi_col_explode(df, cols):
+    """
+    Perform explode on mutliple columns. Same as .explode(['A','B',...]) in newer versions
+    of pandas.
+
+    :param pandas.Dataframe df: Dataframe to explode with cols
+    :param list(str) cols: Name of columns to explode. Underlying series must
+    each be the same length.
+    :return: Exploded dataframe.
+    """
+
+    exploded_cols = []
+    for col in cols:
+        exploded_col = df.pop(col).explode(col)
+        if len(exploded_cols) > 0 and len(exploded_cols[0]) != len(exploded_col):
+            raise ValueError('Cannot explode on columns with different length')
+        exploded_cols.append(exploded_col)
+
+    return df.merge(pd.concat(exploded_cols, axis=1), how='cross')
